7c5d26eb99564fd5a39c886ecf5d7d4ea29ab220
Marton Bur <marton.bur@gmail.com>
2018-10-30 20:40:15 -0400

    Adding solutions to the exercises

--- javascript/exercise.js
+++ javascript/exercise.js
@@ -5,5 +5,12 @@ function getPerson (i) {
           person: data,
           status: response.status}))
   .then(res => {
-    document.getElementById('content').innerHTML=res.person.name}));
+    document.getElementById('content').innerHTML+=res.person.name+"<br/>"}));
+}
+
+function getPersons() {
+  document.getElementById('content').innerHTML=""
+  for (var i = 1; i <= 5; i++) {
+    getPerson(i)
+  }
 }
