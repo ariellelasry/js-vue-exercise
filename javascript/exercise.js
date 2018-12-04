ced7224fda44cfc53091fbd4edbab292e82af60a
ariellelasry <35381480+ariellelasry@users.noreply.github.com>
2018-12-04 15:40:49 -0500

    Update exercise.js

--- javascript/exercise.js
+++ javascript/exercise.js
@@ -1,9 +1,14 @@
 function getPerson (i) {
+  var names = [];
+  for (int i=1; i<6; i++){
+    
   fetch("https://swapi.co/api/people/" + i + "/")
   .then(response =>
       response.json().then(data => ({
           person: data,
           status: response.status}))
   .then(res => {
-    document.getElementById('content').innerHTML=res.person.name}));
+    names.push(res.person.name);
+    document.getElementById('content').innerHTML=res.person.name+","+ document.getElementById('content').innerHTLM}));
+  }
 }
